36475e21823f67d5cae993c367b1decc375741ff
Lili Hoffman <ldhoffma@usc.edu>
2024-04-08 20:25:33 -0700

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Vector;
+
+
+public class Server extends Thread{
+    private int port;
+    private EventParser eventParser;
+    private Map userData;   //Should this be a map???
+	private HashMap dateAndEvent;
+    private HashMap locAndEvent;
+    private static PrintWriter pw;
+	
+	public Server(int port, String hostname) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(port);
+		} catch (IOException ioe) {
+			System.out.println("ioe in ServerThread constructor: " + ioe.getMessage());
+		}
+	}
+
+    //takes in name of an event we are searching for and returns the name of the associated event
+    public Event getEvent(String name){
+
+    }
+
+    //takes in location of an event we are searching by and returns the name of the associated event
+    //How to account for multiple events???
+    public Event getEventByLocation(String loc){
+        
+    }
+
+    //takes in date of an event we are searching by and returns the name of the associated event
+    //How to account for multiple events???
+    public Event getEventByDate(String date){
+        
+    }
+
+	public void sendMessage(String message) {
+		pw.println(message);
+		pw.flush();
+	}
+    
+	public void run() {
+		
+	}
+	
+	public static void main(String[] args) throws ParseException, IOException{
+
+	}
+}
