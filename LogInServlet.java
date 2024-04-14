44ef98eb2c84c50dbbf0ce6bd92230d0ae6fe810
Nora Sun <122490212+ningyues@users.noreply.github.com>
2024-04-14 14:34:09 -0700

    Update LogInServlet.java

--- LogInServlet.java
+++ LogInServlet.java
@@ -1,16 +1,15 @@
+package ningyues_CSCI201_Final_Project;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/LogInServlet")
-public class LogInServlet {
+public class LogInServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
-	
 	
 	// Constructor
 	public LogInServlet() {
@@ -20,15 +19,21 @@ public class LogInServlet {
 	
 	// Code to pull information from log in page
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("login_page/html");
+		response.setContentType("text/html");
 		PrintWriter pw = response.getWriter();
 		String username = request.getParameter("username");
-		String password = request.getParameter("password");
+	    String password = request.getParameter("password");
 		
-		User currUser = new User(username, password);
+//		User currUser = new User(username, password);
 		
 		// TODO: Change to the type of response that the front end can use.
-		pw.print(currUser.authenticate(username, password));
+		//pw.print(currUser.authenticate(username, password));
+	    if (username != null) {
+            pw.println("<h1>Hello, " + username + "</h1>");
+        } else {
+            pw.println("<h1>Hello</h1>");
+        }
+        pw.close();
 	}
 
 	/**
