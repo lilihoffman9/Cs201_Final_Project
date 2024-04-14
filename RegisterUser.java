f79ec90287ef3655de2c806752cb2ec283101348
Janice Lee <jlee7162@usc.edu>
2024-04-14 11:57:23 -0700

    Update RegisterUser.java

--- RegisterUser.java
+++ RegisterUser.java
@@ -1,5 +1,30 @@
 package jlee7162_201_FinalProject;
 
 public class RegisterUser {
+	private String username;
+	private String password;
+	private String email;
+  private String firstName;
+  private String lastName;
+	
+	public RegisterUser(String username, String password, String firstName, String lastName, String email) {
+		this.username = username;
+		this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+		this.email = email;
+	}
+	
+	public void addRegisterUser() {
+		//insert into database
+		return;
+	}
+
+  public String getFirstName(String firstName){
+    return firstName;
+  }
+  public String getLastName(String lastName){
+    return lastName;
+  }
 
 }
