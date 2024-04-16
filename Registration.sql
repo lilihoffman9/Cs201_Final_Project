ed2b5d4a4451a6ddc58e26b56ec8ef8a557ba76b
Nora Sun <122490212+ningyues@users.noreply.github.com>
2024-04-16 15:26:27 -0700

    Update Registration.sql

--- Registration.sql
+++ Registration.sql
@@ -1,9 +1,8 @@
 CREATE TABLE Registration (
-    registrationID VARCHAR(255) PRIMARY KEY,
-    userID VARCHAR(255) NOT NULL,
-    eventName VARCHAR(255) NOT NULL,
-    registrationTime DATETIME NOT NULL,
-    QRCode VARCHAR(255) UNIQUE NOT NULL,
+    registrationID INT AUTO_INCREMENT PRIMARY KEY,
+    userID INT NOT NULL,
+    eventID INT NOT NULL,
+    registrationTime DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY (userID) REFERENCES Users(userID),
-    FOREIGN KEY (eventName) REFERENCES Events(eventName)
+    FOREIGN KEY (eventID) REFERENCES Events(eventID)
 );
