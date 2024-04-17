c6e955e3bc6bf45d4e3990e7b84e54761771df97
Nora Sun <122490212+ningyues@users.noreply.github.com>
2024-04-16 18:28:40 -0700

    Update Users.sql

--- Users.sql
+++ Users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Users (
     userID INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     firstName VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL
 );
