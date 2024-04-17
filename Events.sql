8ca296e52c7e09ab3fddb8fc699f15fa91d1e994
Nora Sun <122490212+ningyues@users.noreply.github.com>
2024-04-16 18:28:22 -0700

    Update Events.sql

--- Events.sql
+++ Events.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Events (
     eventID INT AUTO_INCREMENT PRIMARY KEY,
-    eventName VARCHAR(255) NOT NULL,
+    eventName VARCHAR(255) UNIQUE NOT NULL,
     location VARCHAR(255) NOT NULL,
     date DATETIME NOT NULL,
     description TEXT,
