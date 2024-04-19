9baa16f60d7a2b169d7e63aa4330951f132e9cff
Lili Hoffman <ldhoffma@usc.edu>
2024-04-18 19:43:37 -0700

    Update Messages.java

--- Messages.java
+++ Messages.java
@@ -5,6 +5,44 @@ public class Messages {
     private String message;
     private long timeStamp;
 
+    public void doPost(HttpServletRequest request,HttpServletResponse response){ 
+        try{
+            PrintWriter pw=response.getWriter();
+            response.setContentType("application/json");
+    
+            BufferedReader br = request.getReader();
+            StringBuilder jsonString = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                jsonString.append(line);
+            }
+    
+            Gson gson = new Gson();
+            JsonElement jsonElement = gson.fromJson(jsonString.toString(),JsonElement.class);
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            int senderId = jsonObject.get("senderID").getAsInt();
+            int receiverId = jsonObject.get("receiverID").getAsInt();
+            String message = jsonObject.get("messageContent").getAsString();
+            String timestamp = jsonObject.get("timestamp").getAsString();
+    
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/MessageTable", "username", "password");
+    
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO MessageTable (senderID, receiverID, messageContent, timestamp) VALUES (?, ?, ?, ?)");
+            preparedStatement.setInt(1, senderId);
+            preparedStatement.setInt(2, receiverId);
+            preparedStatement.setString(3, message);
+            preparedStatement.setString(4, timestamp);
+            preparedStatement.executeUpdate();
+    
+            JSONObject responseJson = new JSONObject();
+            responseJson.put("status", "success");
+            pw.print(responseJson.toJSONString());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
     public int getSenderID(){
         return senderId;
     }
