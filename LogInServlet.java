458a210eac3ed2c50843d1d7d544490e13601548
Nora Sun <122490212+ningyues@users.noreply.github.com>
2024-04-21 12:50:42 -0700

    Update LogInServlet.java

--- LogInServlet.java
+++ LogInServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
+import com.google.gson.Gson;
+
 @WebServlet("/LogInServlet")
 public class LogInServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
@@ -14,25 +16,33 @@ public class LogInServlet extends HttpServlet{
 		super();
 	}
 	
-	
 	// Code to pull information from log in page
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		PrintWriter pw = response.getWriter();
-		String username = request.getParameter("username");
-	    String password = request.getParameter("password");
-		
-		User currUser = new User(username, password);
-		
-		// TODO: Change to the type of response that the front end can use.
-		pw.print(currUser.authenticate(username, password));
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter pw = response.getWriter();
+        Gson gson = new Gson();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        if (username == null || password == null) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            pw.write(gson.toJson("User info missing"));
+        } 
+        else {
+        	UserJDBCConnector auth = new UserJDBCConnector();
+        	User curUser = auth.getUser(username, password);
+
+            if (curUser == null) {
+                pw.write(gson.toJson("Invalid login credentials"));
+            } 
+            else { // successfully login
+            	String userJson = gson.toJson(curUser);
+    	        pw.write(userJson);
+            }
+        }
+        pw.flush();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 }
