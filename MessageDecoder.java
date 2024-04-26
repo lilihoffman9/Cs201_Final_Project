80a2b2c480c531ad1b01b2351a1ce1e808494bae
Janice Lee <jlee7162@usc.edu>
2024-04-26 13:46:28 -0700

    Update MessageDecoder.java

--- MessageDecoder.java
+++ MessageDecoder.java
@@ -5,19 +5,25 @@ import jakarta.websocket.DecodeException;
 import jakarta.websocket.Decoder;
 import jakarta.websocket.EndpointConfig;*/
 
-import javax.json.bind.JsonbBuilder;
-import javax.json.bind.JsonbException;
+//import javax.json.bind.JsonbBuilder;
+//import javax.json.bind.JsonbException;
 import javax.websocket.DecodeException;
 import javax.websocket.Decoder;
 import javax.websocket.EndpointConfig;
+
+import com.google.gson.Gson;
 public class MessageDecoder implements Decoder.Text<Message> {
+	Gson gson = new Gson();
 
     public Message decode(String text) throws DecodeException {
-        try {
-            return JsonbBuilder.create().fromJson(text, Message.class);
-        } catch (JsonbException e) {
+       // try {
+        	Message message = new Gson().fromJson(text, Message.class);
+        	return message;
+            //return JsonbBuilder.create().fromJson(text, Message.class);
+            
+         /*catch (JsonbException e) {
             throw new DecodeException(text, e.getMessage(), e);
-        }
+        }*/
     }
 
     public boolean willDecode(String text) {
