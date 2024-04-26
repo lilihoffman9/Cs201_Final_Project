95bc1845018b933767bab714ad8f1e03612d20a8
Janice Lee <jlee7162@usc.edu>
2024-04-26 13:46:46 -0700

    Update MessageEncoder.java

--- MessageEncoder.java
+++ MessageEncoder.java
@@ -8,10 +8,14 @@ import javax.websocket.EncodeException;
 import javax.websocket.Encoder;
 import javax.websocket.EndpointConfig;
 
+import com.google.gson.Gson;
+
 public class MessageEncoder implements Encoder.Text<Message> {
 
     public String encode(Message message) throws EncodeException {
-        return JsonbBuilder.create().toJson(message);
+    	String message1 = new Gson().toJson(message);
+    	return message1;
+       // return JsonbBuilder.create().toJson(message);
     }
 
     public void init(EndpointConfig ec) {
