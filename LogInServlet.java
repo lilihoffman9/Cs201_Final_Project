2717e2244ec7dbe08ef011542ff6946584e45e58
Lili Hoffman <ldhoffma@usc.edu>
2024-04-25 17:42:10 -0700

    Update LogInServlet.java

--- LogInServlet.java
+++ LogInServlet.java
@@ -10,6 +10,7 @@ import com.google.gson.Gson;
 @WebServlet("/LogInServlet")
 public class LogInServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
+	Gson gson;
 	
 	// Constructor
 	public LogInServlet() {
