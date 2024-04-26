b7af33ffea925479fdfdba10a1d21c740d31bb59
Premlatha Premkumar <87746379+pdpremku@users.noreply.github.com>
2024-04-26 13:30:27 -0700

    Update profilePage.js

--- profilePage.js
+++ profilePage.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 
-	var curr_user = JSON.parse(window.localStorage.getItem("curr_user"));
+	var curr_user = JSON.parse(window.localStorage.getItem("user_info"));
 	document.getElementById("my_name").innerHTML += curr_user.username;
 	document.getElementById("username").innerHTML += curr_user.username;
 	document.getElementById("fullname").innerHTML += curr_user.firstName;
