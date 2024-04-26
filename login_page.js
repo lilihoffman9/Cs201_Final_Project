5b8b1da95a2eee12505555c98f657d736b7b6bbe
Nora Sun <122490212+ningyues@users.noreply.github.com>
2024-04-26 13:21:43 -0700

    Update login_page.js

--- login_page.js
+++ login_page.js
@@ -1,27 +1,37 @@
-window.onload = function () {
-
+function login(){
+	console.log("Here");
 	var _username = document.getElementById("username").value;
 	var _password = document.getElementById("password").value;
 	
-	fetch('Messages', {
-		method: 'POST',
-		headers: {'Content-Type': 'application/json'},
-		body: JSON.stringify({
-			username: _username,
-			password: _password
-		})
+	
+	let jsonData = JSON.stringify({
+	    username: _username,
+	    password: _password
+	});
+	
+	
+	console.log("Sending JSON: ", jsonData);
+	fetch('LogInServlet', {
+	    method: 'POST',
+	    headers: {'Content-Type': 'application/json'},
+	    body: jsonData
 	})
 	.then(res => res.json())
 	.then((data) => {
 	  if(data == "Invalid login credentials")
 	  {
+		alert("Invalid login credentials");
 	  	console.log("Error log!");
+	  }
+	  else if (data == "User info missing") {
+		  alert("User info missing");
 	  }
 	  else
 	  {
 	  	window.localStorage.setItem("user_info", data);
 	  	window.location.href = 'profilePage.html';
 	  	console.log("Received upon login: " + data);
+	  	
 	  }
 	})
 	.catch(error => {
