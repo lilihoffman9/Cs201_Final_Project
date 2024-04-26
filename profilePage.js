b28837fb6a58ea71fedc40cab758a1f6f29610cd
Nora Sun <122490212+ningyues@users.noreply.github.com>
2024-04-26 13:58:37 -0700

    Update profilePage.js

--- profilePage.js
+++ profilePage.js
@@ -1,11 +1,12 @@
 window.onload = function () {
 
-	var curr_user = JSON.parse(window.localStorage.getItem("user_info"));
-	document.getElementById("my_name").innerHTML += curr_user.username;
-	document.getElementById("username").innerHTML += curr_user.username;
-	document.getElementById("fullname").innerHTML += curr_user.firstName;
-	document.getElementById("fullname").innerHTML += curr_user.lastName;
-	document.getElementById("email").innerHTML += curr_user.email;
+	var curr_user = localStorage.getItem("user_info");
+	console.log(JSON.stringify(curr_user));
+	document.getElementById("userName").innerHTML += localStorage.getItem("userName");
+	document.getElementById("username").innerHTML += localStorage.getItem("userName");
+	document.getElementById("fullname").innerHTML += localStorage.getItem("firstName");
+	document.getElementById("fullname").innerHTML += localStorage.getItem("lastName");
+	document.getElementById("email").innerHTML += localStorage.getItem("email");
 	
 	return false;
 }
