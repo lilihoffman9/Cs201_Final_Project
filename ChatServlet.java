11e35bb6b75851917d0c8d076110f1c90879b9b6
Lakshya Desai <ldesai@usc.edu>
2024-05-03 22:35:57 -0700

    Update ChatServlet.java

--- ChatServlet.java
+++ ChatServlet.java
@@ -1,44 +1,30 @@
-
-
-import java.io.IOException;
 import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import javax.websocket.Session;
 import javax.websocket.OnOpen;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 
-
-
-/**
- * Servlet implementation class ChatServlet
- */
 @ServerEndpoint("/ChatServlet")
-public class ChatServlet extends HttpServlet {
-	private static Set<Session> userSessions = Collections.newSetFromMap(new ConcurrentHashMap<Session, Boolean>());
-	
-	@OnOpen
-	public void OnOpen(Session curSession) {
-		userSessions.add(curSession);
-	}
-	
-	@OnClose
-	public void OnClose(Session curSession) {
-		userSessions.remove(curSession);
-	}
-	
-	@OnMessage
-	public void onMessage(String message, Session userSession) {
-		for(Session ses: userSessions) {
-			ses.getAsyncRemote().sendText(message);
-		}
-	}
+public class ChatServlet {
+    private static Set<Session> userSessions = Collections.newSetFromMap(new ConcurrentHashMap<Session, Boolean>());
+
+    @OnOpen
+    public void onOpen(Session curSession) {
+        userSessions.add(curSession);
+    }
+
+    @OnClose
+    public void onClose(Session curSession) {
+        userSessions.remove(curSession);
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session userSession) {
+        for(Session ses: userSessions) {
+            ses.getAsyncRemote().sendText(message);
+        }
+    }
 }
