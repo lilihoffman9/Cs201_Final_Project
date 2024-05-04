349ff70f10b85b1d7843cf54689d846d77c02539
Lakshya Desai <ldesai@usc.edu>
2024-05-04 12:45:13 -0700

    Update ChatServlet.java

--- ChatServlet.java
+++ ChatServlet.java
@@ -1,30 +1,41 @@
+import java.io.IOException;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import javax.websocket.server.ServerEndpoint;
-import javax.websocket.Session;
-import javax.websocket.OnOpen;
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/ChatServlet")
-public class ChatServlet {
-    private static Set<Session> userSessions = Collections.newSetFromMap(new ConcurrentHashMap<Session, Boolean>());
+@ServerEndpoint("/chat")
+public class ChatEndpoint {
+    private static Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
 
     @OnOpen
-    public void onOpen(Session curSession) {
-        userSessions.add(curSession);
-    }
-
-    @OnClose
-    public void onClose(Session curSession) {
-        userSessions.remove(curSession);
+    public void onOpen(Session session) {
+        sessions.add(session);
     }
 
     @OnMessage
-    public void onMessage(String message, Session userSession) {
-        for(Session ses: userSessions) {
-            ses.getAsyncRemote().sendText(message);
+    public void onMessage(String message, Session session) throws IOException {
+        // Broadcasting the message to all sessions
+        for (Session s : sessions) {
+            if (s.isOpen()) {
+                s.getAsyncRemote().sendText(message);
+            }
         }
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        sessions.remove(session);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        // Log or handle error
+        System.err.println("Error on WebSocket session: " + throwable.getMessage());
     }
 }
